c66ad07379a82e79bfc01d136436796d1497c4d2
Eng-DO4 <mustafayasser1212@gmail.com>
2022-12-27 15:02:50 +0200

    fix bad concepts and bugs

--- learnCPP-main/basics/syntax/escapeChar/test.cpp
+++ learnCPP-main/basics/syntax/escapeChar/test.cpp
@@ -4,14 +4,23 @@ using namespace std;
 
 int main()
 {
- cout << "one\n"; // print a new line
- cout << "two\\\"three\"\n"; // print the char after \\
- // if u use one backslach at the end of a line it causes err
- // if u use \" at the end of cout statement instead of \"" it causes err
- cout << "\tfour\n"; // print one tap
- cout << "\'five\*\n"; // print whatever special char u want
- // Note that u can't use escapeChar method without be in ""
- cout << "six6\b"; // simulate backspace button on keyboard
- cout << "seven\a\n"; // print alert
- cout << "eight\rnine"; // make whatever after at the first
+    // Note that u can't use escapeChar method without be in ""
+    // if u use \ at the end of a line it causes err if the next line begin with code
+    // if u use \ at the end of a line ends with a comment it comments the next line even if it is code
+    // if u use \" at the end of cout statement instead of \"" it causes err cuz escaping "
+
+    cout << "one\n"; // print a new line
+
+    cout << "two\\\'three\"\n"; // print the char after \ this 3 chars => (\ - ' - ")
+
+    cout << "\tfour\n"; // print one tap (8 spaces)
+
+    cout << "six6\b"; // simulate backspace button on keyboard (remove the prev char in this line)
+
+    cout << "seven\a\n"; // print alert => (I can't see the result here 😅)
+
+    cout << "0123456789"; // any special char can be written in quotes except (\ - " - ')
+    cout << "eight nine ten \releven twelve\n"; // replace whatever after with the first ame no of chars in this line
+
+    return 0;
 }
